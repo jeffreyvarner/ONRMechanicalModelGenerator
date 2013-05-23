6005c327dc26d3b0e3dbae9bca9e270638b09e20
jeffreyvarner <jeff@curveos.com>
2013-05-23 08:46:17 -0400

    Project commit

---
+++ ONRMechanicalModelGenerator/ONRMechanicalModelGenerator/VLDocument.h
@@ -0,0 +1,13 @@
+//
+//  VLDocument.h
+//  ONRMechanicalModelGenerator
+//
+//  Created by Jeffrey Varner on 5/23/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VLDocument : NSDocument
+
+@end
---
+++ ONRMechanicalModelGenerator/ONRMechanicalModelGenerator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ONRMechanicalModelGenerator
+//
+//  Created by Jeffrey Varner on 5/23/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
